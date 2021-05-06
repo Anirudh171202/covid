bcb5e01382e6ec1f908d60fe2af0cc8177ee0512
vrishk <krishnavrishab@gmail.com>
2021-05-06 11:54:49 +0530

    feat: scrape all data

--- preprocessing.py
+++ preprocessing.py
@@ -5,17 +5,17 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.preprocessing import StandardScaler
 
-import pandas as pd 
+import pandas as pd
 import json
 import random
 from numpy import mean, std
 import numpy as np
 import timeit
 
-#DATA MANIPULATION
+# DATA MANIPULATION
 vectorizer = CountVectorizer(
-    analyzer = 'word',
-    lowercase = False,
+    analyzer='word',
+    lowercase=False,
 )
 
 data = []
@@ -26,68 +26,71 @@ with open("text.json", "r+") as f:
         data.extend(obj[i])
         labels.extend([i]*(len(obj[i])))
 
-#SHUFFLE
+# SHUFFLE
 
 c = list(zip(data, labels))
 
 random.shuffle(c)
-data, labels  = zip(*c)
+data, labels = zip(*c)
 
-#PREPROCESSING
+# PREPROCESSING
 features = vectorizer.fit_transform(
     data
 )
 
 features_nd = features.toarray()
 
-X_train_og, X_test_og, y_train, y_test  = train_test_split(
-        features_nd, 
-        labels,
-        train_size=0.80, 
-        random_state=1234)
+X_train_og, X_test_og, y_train, y_test = train_test_split(
+    features_nd,
+    labels,
+    train_size=0.80,
+    random_state=1234)
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train_og)
 X_test = scaler.transform(X_test_og)
 
-#LOGISTIC REGRESSION (85% accuracy)
+# LOGISTIC REGRESSION (85% accuracy)
 starttime = timeit.default_timer()
 log_model = LogisticRegression()
 log_model = log_model.fit(X=X_train, y=y_train)
 y_pred = log_model.predict(X_test)
-print("Accuracy for logistic : ", accuracy_score(y_test, y_pred), "\ntime taken ",  str(timeit.default_timer() - starttime)[:4],  "s\n")
+print("Accuracy for logistic : ", accuracy_score(y_test, y_pred),
+      "\ntime taken ",  str(timeit.default_timer() - starttime)[:4],  "s\n")
 
-#RANDOM FORREST (90% accuracy)
+# RANDOM FOREST (90% accuracy)
 starttime = timeit.default_timer()
-clf=RandomForestClassifier(n_estimators=150)
-clf.fit(X_train,y_train)
-y_pred=clf.predict(X_test)
+clf = RandomForestClassifier(n_estimators=150)
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
 
-#ACCURACY FOR LOGISTIC/RANDOM FORREST
-print("Accuracy for random forrest : ", accuracy_score(y_test, y_pred), "\ntime taken : ",  str(timeit.default_timer() - starttime)[:4], "s\n")
+# ACCURACY FOR LOGISTIC/RANDOM FOREST
+print("Accuracy for random forest : ", accuracy_score(y_test, y_pred),
+      "\ntime taken : ",  str(timeit.default_timer() - starttime)[:4], "s\n")
 
 y_pred = clf.predict(X_test)
 
 # NICE TABLE FOR SEEING TESTING RESULTS
-reversefactor = dict(zip(["need", "give", "useless"],["NEED", "GIVE", "USELESS"]))
+reversefactor = dict(zip(["need", "give", "useless"],
+                         ["NEED", "GIVE", "USELESS"]))
 y_test = np.vectorize(reversefactor.get)(y_test)
 y_pred = np.vectorize(reversefactor.get)(y_pred)
 # Making the Confusion Matrix
 
-print(pd.crosstab(y_test, y_pred, rownames=['Actual Classification'], colnames=['Predicted CLASSIFICATION']))
+print(pd.crosstab(y_test, y_pred, rownames=[
+      'Actual Classification'], colnames=['Predicted CLASSIFICATION']))
 print()
 # #ADABOOST (75% accuracy)
 # starttime = timeit.default_timer()
 # model = AdaBoostClassifier()
 # cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
 # n_scores = cross_val_score(model, X_train, y_train, scoring='accuracy', cv=cv, n_jobs=-1, error_score='raise')
-# 
-#ACCURACY FOR ADABOOST
+#
+# ACCURACY FOR ADABOOST
 # print('Accuracy for adaboost: %.3f (%.3f)' % (mean(n_scores), std(n_scores)), "\ntime taken : ", str(timeit.default_timer() - starttime)[:5], "s\n")
 
-#CODE FOR PRINTING INPUTS WITH OUTPUTS FOR DEBUGGING
+# CODE FOR PRINTING INPUTS WITH OUTPUTS FOR DEBUGGING
 for i in [random.randint(0, len(X_test_og)) for i in range(5)]:
     print(y_pred[i])
     ind = features_nd.tolist().index(X_test_og[i].tolist())
     print(data[ind].strip())
     print()
-
--- scrape.sh
+++ scrape.sh
@@ -1,11 +1,10 @@
 #! /bin/sh
 
-# items=("covid icu" "hospital" "oxygen" "concentrator" "ambulance" "ventilator")
-items=("covid icu")
+items=("covid icu" "hospital" "oxygen" "concentrator" "ambulance" "ventilator")
 
 # TODO Use no retweet, language translate, etc in twint
 
 for i in "${items[@]}"; do
     echo "$i"
-    ~/.local/bin/twint --near Bangalore --since 2021-05-01 --search "$i" --json -o input.json >/dev/null
+    ~/.local/bin/twint --filter-retweets --near Bangalore --since 2021-05-01 --search "$i" --json -o input.json >/dev/null
 done
