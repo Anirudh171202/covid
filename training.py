d7e99f5e29679e540710e9ff8bb0b9b6819575ef
vrishk <krishnavrishab@gmail.com>
2021-05-09 10:17:05 +0530

    try: balanced random forest - 89%

--- training.py
+++ training.py
@@ -1,23 +1,22 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split, RepeatedStratifiedKFold, cross_val_score
-from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+from imblearn.ensemble import BalancedRandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 import pickle
-import pandas as pd 
+import pandas as pd
 import json
 import random
 from numpy import mean, std
 import numpy as np
 import timeit
 
-#DATA MANIPULATION
+# DATA MANIPULATION
 vectorizer = CountVectorizer(
-    analyzer = 'word',
-    lowercase = False,
+    analyzer='word',
+    lowercase=False,
 )
-
 
 
 data = []
@@ -28,14 +27,14 @@ with open("text.json", "r+") as f:
         data.extend(obj[i])
         labels.extend([i]*(len(obj[i])))
 
-#SHUFFLE
+# SHUFFLE
 
 c = list(zip(data, labels))
 
 random.shuffle(c)
-data, labels  = zip(*c)
+data, labels = zip(*c)
 
-#PREPROCESSING
+# PREPROCESSING
 features = vectorizer.fit_transform(
     data
 )
@@ -43,11 +42,11 @@ features = vectorizer.fit_transform(
 print("vectorizer shape", features.shape)
 features_nd = features.toarray()
 
-X_train_og, X_test_og, y_train, y_test  = train_test_split(
-        features_nd, 
-        labels,
-        train_size=0.75, 
-        random_state=1234)
+X_train_og, X_test_og, y_train, y_test = train_test_split(
+    features_nd,
+    labels,
+    train_size=0.75,
+    random_state=1234)
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train_og)
 X_test = scaler.transform(X_test_og)
@@ -58,36 +57,39 @@ X_test = scaler.transform(X_test_og)
 # log_model = log_model.fit(X=X_train, y=y_train)
 # y_pred = log_model.predict(X_test)
 # print("Accuracy for logistic : ", accuracy_score(y_test, y_pred), "\ntime taken ",  str(timeit.default_timer() - starttime)[:4],  "s\n")
-# 
-#RANDOM FORREST (90% accuracy)
+#
+# RANDOM FORREST (90% accuracy)
 starttime = timeit.default_timer()
-clf=RandomForestClassifier(n_estimators=150)
-clf.fit(X_train,y_train)
+clf = BalancedRandomForestClassifier(n_estimators=150)
+clf.fit(X_train, y_train)
 print(X_train.shape)
-y_pred=clf.predict(X_test)
+y_pred = clf.predict(X_test)
 
-#ACCURACY FOR LOGISTIC/RANDOM FORREST
-print("Accuracy for random forrest : ", accuracy_score(y_test, y_pred), "\ntime taken : ",  str(timeit.default_timer() - starttime)[:4], "s\n")
+# ACCURACY FOR LOGISTIC/RANDOM FORREST
+print("Accuracy for random forrest : ", accuracy_score(y_test, y_pred),
+      "\ntime taken : ",  str(timeit.default_timer() - starttime)[:4], "s\n")
 y_pred = clf.predict(X_test)
 
 # NICE TABLE FOR SEEING TESTING RESULTS
-reversefactor = dict(zip(["need", "give", "useless"],["NEED", "GIVE", "USELESS"]))
+reversefactor = dict(zip(["need", "give", "useless"],
+                         ["NEED", "GIVE", "USELESS"]))
 y_test = np.vectorize(reversefactor.get)(y_test)
 y_pred = np.vectorize(reversefactor.get)(y_pred)
 # Making the Confusion Matrix
 
-print(pd.crosstab(y_test, y_pred, rownames=['Actual Classification'], colnames=['Predicted CLASSIFICATION']))
+print(pd.crosstab(y_test, y_pred, rownames=[
+      'Actual Classification'], colnames=['Predicted CLASSIFICATION']))
 print()
 # #ADABOOST (75% accuracy)
 # starttime = timeit.default_timer()
 # model = AdaBoostClassifier()
 # cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
 # n_scores = cross_val_score(model, X_train, y_train, scoring='accuracy', cv=cv, n_jobs=-1, error_score='raise')
-# 
-#ACCURACY FOR ADABOOST
+#
+# ACCURACY FOR ADABOOST
 # print('Accuracy for adaboost: %.3f (%.3f)' % (mean(n_scores), std(n_scores)), "\ntime taken : ", str(timeit.default_timer() - starttime)[:5], "s\n")
 
-#CODE FOR PRINTING INPUTS WITH OUTPUTS F OR DEBUGGING
+# CODE FOR PRINTING INPUTS WITH OUTPUTS F OR DEBUGGING
 
 for i in [random.randint(0, len(X_test_og)) for i in range(5)]:
     print(y_pred[i])
