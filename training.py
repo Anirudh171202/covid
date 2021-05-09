817337833db449c008b9fa92c2973546141bbc78
vrishk <krishnavrishab@gmail.com>
2021-05-09 12:00:28 +0530

    chore: clean code, 89.2 acc

--- training.py
+++ training.py
@@ -1,23 +1,14 @@
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split, RepeatedStratifiedKFold, cross_val_score
-from sklearn.metrics import accuracy_score, confusion_matrix
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, RepeatedStratifiedKFold
+from sklearn.metrics import accuracy_score
 from imblearn.ensemble import BalancedRandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 import pickle
 import pandas as pd
 import json
 import random
-from numpy import mean, std
 import numpy as np
 import timeit
-
-# DATA MANIPULATION
-vectorizer = CountVectorizer(
-    analyzer='word',
-    lowercase=False,
-)
-
 
 data = []
 labels = []
@@ -27,23 +18,30 @@ with open("text.json", "r+") as f:
         data.extend(obj[i])
         labels.extend([i]*(len(obj[i])))
 
-# SHUFFLE
-
 c = list(zip(data, labels))
+
+# Preprocessors
+vectorizer = CountVectorizer(
+    analyzer='word',
+    lowercase=False,
+)
+
+
+# Shuffle
 
 random.shuffle(c)
 data, labels = zip(*c)
 
-# PREPROCESSING
+# Get features
 features = vectorizer.fit_transform(
     data
 )
 
-print("vectorizer shape", features.shape)
-features_nd = features.toarray()
+print("Features shape:", features.shape)
+features = features.toarray()
 
 X_train_og, X_test_og, y_train, y_test = train_test_split(
-    features_nd,
+    features,
     labels,
     train_size=0.75,
     random_state=1234)
@@ -51,52 +49,36 @@ scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train_og)
 X_test = scaler.transform(X_test_og)
 
-# #LOGISTIC REGRESSION (85% accuracy)
-# starttime = timeit.default_timer()
-# log_model = LogisticRegression()
-# log_model = log_model.fit(X=X_train, y=y_train)
-# y_pred = log_model.predict(X_test)
-# print("Accuracy for logistic : ", accuracy_score(y_test, y_pred), "\ntime taken ",  str(timeit.default_timer() - starttime)[:4],  "s\n")
-#
-# RANDOM FORREST (90% accuracy)
+# Balanced Random Forest
+
 starttime = timeit.default_timer()
 clf = BalancedRandomForestClassifier(n_estimators=150)
 clf.fit(X_train, y_train)
-print(X_train.shape)
+print("Time: ",  str(timeit.default_timer() - starttime)[:4])
+
+# Accuracy
+y_pred = clf.predict(X_test)
+print("Accuracy:", accuracy_score(y_test, y_pred))
 y_pred = clf.predict(X_test)
 
-# ACCURACY FOR LOGISTIC/RANDOM FORREST
-print("Accuracy for random forrest : ", accuracy_score(y_test, y_pred),
-      "\ntime taken : ",  str(timeit.default_timer() - starttime)[:4], "s\n")
-y_pred = clf.predict(X_test)
-
-# NICE TABLE FOR SEEING TESTING RESULTS
+# Confusion Matrix
 reversefactor = dict(zip(["need", "give", "useless"],
                          ["NEED", "GIVE", "USELESS"]))
 y_test = np.vectorize(reversefactor.get)(y_test)
 y_pred = np.vectorize(reversefactor.get)(y_pred)
-# Making the Confusion Matrix
 
 print(pd.crosstab(y_test, y_pred, rownames=[
-      'Actual Classification'], colnames=['Predicted CLASSIFICATION']))
+      'Actual'], colnames=['Predicted']))
 print()
-# #ADABOOST (75% accuracy)
-# starttime = timeit.default_timer()
-# model = AdaBoostClassifier()
-# cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
-# n_scores = cross_val_score(model, X_train, y_train, scoring='accuracy', cv=cv, n_jobs=-1, error_score='raise')
-#
-# ACCURACY FOR ADABOOST
-# print('Accuracy for adaboost: %.3f (%.3f)' % (mean(n_scores), std(n_scores)), "\ntime taken : ", str(timeit.default_timer() - starttime)[:5], "s\n")
 
-# CODE FOR PRINTING INPUTS WITH OUTPUTS F OR DEBUGGING
-
+# Sample predictions
 for i in [random.randint(0, len(X_test_og)) for i in range(5)]:
     print(y_pred[i])
-    ind = features_nd.tolist().index(X_test_og[i].tolist())
+    ind = features.tolist().index(X_test_og[i].tolist())
     print(data[ind].strip())
     print()
 
 
+# Saving
 with open('model.pkl', 'wb') as model_file:
     pickle.dump(clf, model_file)
